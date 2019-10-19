393f0497bc39a0d708be6c0bc908bc85b8a5e4b7
donnierisk <donnierisk@gmail.com>
2019-10-19 18:12:20 +0200

    abstract class added

--- src/models/AbstractLiving.ts
+++ src/models/AbstractLiving.ts
@@ -1,12 +1,29 @@
 export default abstract class AbstractLiving {
-  private hp: number;
-  private attack: number;
-  private name: string;
-  private description: string = "";
-  private isHostile: boolean = false;
-  constructor(hp: number, attack: number, name: string) {
-    this.hp = hp;
-    this.attack = attack;
+  private hp: Hitpoints
+  private attack: number
+  private name: string
+  private description: string = ""
+  private isHostile: boolean = false
+  private movement: string[] = []
+  constructor(hp: Hitpoints, attack: number, name: string) {
+    this.hp = hp
+    this.attack = attack
     this.name = name
   }
+
+  public Damage(points: number) {
+    this.hp.current -= points
+    this.hp.current = this.hp.current < 0 ? 0 : this.hp.current
+  }
+
+  public Heal(points: number) {
+    this.hp.current += points
+    this.hp.current = this.hp.current > this.hp.max ? this.hp.max : this.hp.current
+  }
+
+}
+
+interface Hitpoints {
+  max: number
+  current: number
 }
