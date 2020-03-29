5f7a0d9b6310caf6d1dbb9b8b1f2c8071ea02626
Donovan Risk <donnierisk@gmail.com>
2020-03-29 17:42:59 +0200

    removed extra config

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,6 @@
 module.exports = {
     // ...other vue-cli plugin options...
     pwa: {
-        workboxPluginMode: "InjectManifest",
-        workboxOptions: {
-            swSrc: "src/registerServiceWorker.ts",
-        },
-        name: "Louwrisk",
-        appleMobileWebAppCapable: true,
-        themeColor: "#B1B2B4",
-        msTileColor: "#B1B2B4",
         manifestOptions: {
             orientation: "landscape",
             display: "fullscreen",
